bace8ef2bf7d4be2e9340ca232e1924f2352a0bb
Abhishek Mishra <ideamonk@gmail.com>
2012-10-20 02:19:53 +0530

    trying out ghoseb/dotemacs

--- init.el
+++ init.el
@@ -3,104 +3,13 @@
 ;; This is where everything starts. Do you remember this place?
 ;; It remembers you...
 
-(add-to-list 'command-switch-alist
-             (cons "--live-safe-mode"
-                   (lambda (switch)
-                     nil)))
-
-(setq live-safe-modep
-      (if (member "--live-safe-mode" command-line-args)
-          "debug-mode-on"
-        nil))
-
-(setq initial-scratch-message "
-;; I'm sorry, Emacs Live failed to start correctly.
-;; Hopefully the issue will be simple to resolve.
-;;
-;; First up, could you try running Emacs Live in safe mode:
-;;
-;;    emacs --live-safe-mode
-;;
-;; This will only load the default packs. If the error no longer occurs
-;; then the problem is probably in a pack that you are loading yourself.
-;; If the problem still exists, it may be a bug in Emacs Live itself.
-;;
-;; In either case, you should try starting Emacs in debug mode to get
-;; more information regarding the error:
-;;
-;;    emacs --debug-init
-;;
-;; Please feel free to raise an issue on the Gihub tracker:
-;;
-;;    https://github.com/overtone/emacs-live/issues
-;;
-;; Alternatively, let us know in the mailing list:
-;;
-;;    http://groups.google.com/group/overtone
-;;
-;; Good luck, and thanks for using Emacs Live!
-;;
-;;                _.-^^---....,,--
-;;            _--                  --_
-;;           <          SONIC         >)
-;;           |       BOOOOOOOOM!       |
-;;            \._                   _./
-;;               ```--. . , ; .--'''
-;;                     | |   |
-;;                  .-=||  | |=-.
-;;                  `-=#$%&%$#=-'
-;;                     | ;  :|
-;;            _____.,-#%&$@%#&#~,._____
-;;      May these instructions help you raise
-;;                  Emacs Live
-;;                from the ashes
-")
-
-(setq live-supported-emacsp t)
-
-(when (< emacs-major-version 24)
-  (setq live-supported-emacsp nil)
-  (setq initial-scratch-message (concat "
-;;                _.-^^---....,,--
-;;            _--                  --_
-;;           <          SONIC         >)
-;;           |       BOOOOOOOOM!       |
-;;            \._                   _./
-;;               ```--. . , ; .--'''
-;;                     | |   |
-;;                  .-=||  | |=-.
-;;                  `-=#$%&%$#=-'
-;;                     | ;  :|
-;;            _____.,-#%&$@%#&#~,._____
-;;
-;; I'm sorry, Emacs Live is only supported on Emacs 24+.
-;;
-;; You are running: " emacs-version "
-;;
-;; Please upgrade your Emacs for full compatibility.
-;;
-;; Latest versions of Emacs can be found here:
-;;
-;; OS X GUI     - http://emacsformacosx.com/
-;; OS X Console - via homebrew (http://mxcl.github.com/homebrew/)
-;;                brew install emacs
-;; Windows      - http://alpha.gnu.org/gnu/emacs/windows/
-;; Linux        - Consult your package manager or compile from source
-
-"))
-  (let* ((old-file (concat (file-name-as-directory "~") ".emacs-old.el")))
-    (if (file-exists-p old-file)
-      (load-file old-file)
-      (error (concat "Oops - your emacs isn't supported. Emacs Live only works on Emacs 24+ and you're running version: " emacs-version ". Please upgrade your Emacs and try again, or define ~/.emacs-old.el for a fallback")))))
-
-(when live-supported-emacsp
 ;; Store live base dirs
-(setq live-root-dir user-emacs-directory)
+(setq live-root-dir (file-name-directory
+                     (or (buffer-file-name) load-file-name)))
 
 (setq
  live-tmp-dir      (file-name-as-directory (concat live-root-dir "tmp"))
  live-etc-dir      (file-name-as-directory (concat live-root-dir "etc"))
- live-lib-dir      (file-name-as-directory (concat live-root-dir "lib"))
  live-packs-dir    (file-name-as-directory (concat live-root-dir "packs"))
  live-autosaves-dir(file-name-as-directory (concat live-tmp-dir  "autosaves"))
  live-backups-dir  (file-name-as-directory (concat live-tmp-dir  "backups"))
@@ -112,58 +21,77 @@
 (make-directory live-autosaves-dir t)
 (make-directory live-backups-dir t)
 
-;; Load manifest
-(load-file (concat live-root-dir "manifest.el"))
-
-;; load live-lib
-(load-file (concat live-lib-dir "live-core.el"))
-
-;;default live packs
-(let* ((live-dir (file-name-as-directory "live")))
-  (setq live-packs (list (concat live-dir "foundation-pack")
-                         (concat live-dir "colour-pack")
-                         (concat live-dir "clojure-pack")
-                         (concat live-dir "lang-pack")
-                         (concat live-dir "power-pack"))))
-
 ;; Helper fn for loading live packs
 
-(defun live-version ()
-  (interactive)
-  (if (called-interactively-p 'interactive)
-      (message "%s" (concat "This is Emacs Live " live-version))
-    live-version))
+(defun live-pack-config-dir ()
+  "Returns the path of the config dir for the current pack"
+  (file-name-as-directory (concat live-load-pack-dir "config")))
 
+(defun live-pack-lib-dir ()
+  "Returns the path of the lib dir for the current pack"
+  (file-name-as-directory (concat live-load-pack-dir "lib")))
+
+(defun live-load-pack (pack-name)
+  "Load a live pack. This is a dir that contains at least a file
+  called init.el. Adds the packs's lib dir to the load-path"
+  (let* ((pack-name (if (symbolp pack-name)
+                        (symbol-name pack-name)
+                      pack-name))
+         (pack-name-dir (if (file-name-absolute-p pack-name)
+                            (file-name-as-directory pack-name)
+                          (file-name-as-directory (concat live-packs-dir pack-name))))
+         (pack-init (concat pack-name-dir "init.el")))
+    (setq live-load-pack-dir pack-name-dir)
+    (add-to-list 'load-path (live-pack-lib-dir))
+    (if (file-exists-p pack-init)
+        (load-file pack-init))
+    (setq live-load-pack-dir nil)))
+
+(defun live-add-pack-lib (p)
+  "Adds the path (specified relative to the the pack's lib dir)
+  to the load-path"
+  (add-to-list 'load-path (concat (live-pack-lib-dir) p)))
+
+(defun live-load-config-file (f-name)
+  "Load the config file with name f-name in the current pack"
+  (let* ((config-dir (live-pack-config-dir)))
+    (load-file (concat config-dir f-name))))
+
+(defun live-use-packs (pack-list)
+  "Use the packs in pack-list - overrides the defaults and any
+  previous packs added with live-add-packs."
+  (setq live-packs pack-list))
+
+(defun live-add-packs (pack-list)
+  "Add the list pack-list to end of the current list of packs to
+  load"
+  (setq live-packs (append live-packs pack-list)))
 
 ;; Load `~/.emacs-live.el`. This allows you to override variables such
 ;; as live-packs (allowing you to specify pack loading order)
-;; Does not load if running in safe mode
-(let* ((pack-file (concat (file-name-as-directory "~") ".emacs-live.el")))
-  (if (and (file-exists-p pack-file) (not live-safe-modep))
-      (load-file pack-file)))
+;; (let* ((pack-file (concat (file-name-as-directory "~") ".emacs-live.el")))
+;;   (if (file-exists-p pack-file)
+;;       (load-file pack-file)))
 
-;; Load all packs - Power Extreme!
-(mapcar (lambda (pack-dir)
-          (live-load-pack pack-dir))
-        (live-pack-dirs))
+(defun live-load-all-packs (live-packs)
+  (mapcar (lambda (pack-name) (live-load-pack pack-name)) live-packs)
+  ; user is last
+  (live-load-pack "user"))
+
+(defun live-user-first-name ()
+  (car  (split-string user-full-name)))
 
 (setq live-welcome-messages
-      (if (live-user-first-name-p)
-          (list (concat "Hello " (live-user-first-name) ", somewhere in the world the sun is shining for you right now.")
-                (concat "Hello " (live-user-first-name) ", it's lovely to see you again. I do hope that you're well.")
-                (concat (live-user-first-name) ", turn your head towards the sun and the shadows will fall behind you.")
-                )
-        (list  "Hello, somewhere in the world the sun is shining for you right now."
-               "Hello, it's lovely to see you again. I do hope that you're well."
-               "Turn your head towards the sun and the shadows will fall behind you.")))
-
+      (list (concat "Hello " (live-user-first-name) ", somewhere in the world the Sun is shining for you right now.")
+            (concat "Hello " (live-user-first-name) ", it's lovely to see you again. I do hope that you're well.")
+            (concat (live-user-first-name) ", this could be the beginnings of a great hacking session.")
+            (concat (live-user-first-name) ", may the source be with you; the force, aka GNU Emacs is at your command.")
+            (concat (live-user-first-name) ", turn your head towards the Sun and the shadows will fall behind you.")))
 
 (defun live-welcome-message ()
   (nth (random (length live-welcome-messages)) live-welcome-messages))
 
-(when live-supported-emacsp
-  (setq initial-scratch-message (concat ";;
-;;     MM\"\"\"\"\"\"\"\"`M
+(setq initial-scratch-message (concat ";;     MM\"\"\"\"\"\"\"\"`M
 ;;     MM  mmmmmmmM
 ;;     M`      MMMM 88d8b.d8b. .d8888b. .d8888b. .d8888b.
 ;;     MM  MMMMMMMM 88''88'`88 88'  `88 88'  `\"\" Y8ooooo.
@@ -177,16 +105,20 @@
 ;;         M  MMMMMMMM M  M M  MMMM' .M MM  MMMMMMMM
 ;;         M  MMMMMMMM M  M M  MMP' .MM MM  MMMMMMMM
 ;;         M         M M  M M     .dMMM MM        .M
-;;         MMMMMMMMMMM MMMM MMMMMMMMMMM MMMMMMMMMMMM  Version " live-version
-                                                                (if live-safe-modep
-                                                                    "
-;;                                                     --*SAFE MODE*--"
-                                                                  "
-;;"
-                                                                  ) "
-;;           http://github.com/overtone/emacs-live
+;;         MMMMMMMMMMM MMMM MMMMMMMMMMM MMMMMMMMMMMM
 ;;
-;; "                                                      (live-welcome-message) "
+;;           http://github.com/ghoseb/dotemacs
+;;
+;; " (live-welcome-message) "
 
-")))
-)
+"))
+
+;;;
+;;; Entry point
+;;;
+
+;;default live packs
+(setq live-packs '("core" "themes" "power" "clojure" "programming" "notes"))
+
+;; Load all packs - Power Extreme!
+(live-load-all-packs live-packs)
