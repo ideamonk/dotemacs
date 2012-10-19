2772cd0913c736b8eea4479abfed0af89eeeacfa
Abhishek Mishra <ideamonk@gmail.com>
2012-10-20 04:10:18 +0530

    won't be needing this for simple shell

--- packs/user/init.el
+++ packs/user/init.el
@@ -10,12 +10,5 @@
 
 (set-face-background 'default "#161718")
 
-;; fix the PATH variable, source -  http://goo.gl/qeLnC
-;; (defun set-exec-path-from-shell-PATH ()
-;;   (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
-;;     (setenv "PATH" path-from-shell)
-;;     (setq exec-path (split-string path-from-shell path-separator))))
-;; (if window-system (set-exec-path-from-shell-PATH))
-
 ;; enable user and host specific configs
 (load (concat user-emacs-directory system-name ".el"))
